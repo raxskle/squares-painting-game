7fda6ece95fa58487137d36e761cb047756c242b
raxskle <3328147378@qq.com>
2022-08-23 00:24:01 +0800

    Test1.1 nearly perfect resizing

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
 
-module.exports = defineConfig({
+let config = {
   transpileDependencies: true,
-  devServer: {
-    host: "192.168.0.105",
-    port: 8081,
-    setupMiddlewares: require("./mock/testMock.js"),
-  },
-});
+};
+
+if (process.env.VUE_APP_MOCK == "true") {
+  config = {
+    transpileDependencies: true,
+    devServer: {
+      host: "192.168.0.105",
+      port: 8081,
+      setupMiddlewares: require("./mock/testMock.js"),
+    },
+  };
+}
+
+module.exports = defineConfig(config);
