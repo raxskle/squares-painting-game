8e98b35533bbd915bf9dd2760858ba85a7658fbf
raxskle <3328147378@qq.com>
2022-08-23 16:14:14 +0800

    api

--- src/request/axios.js
+++ src/request/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 if (process.env.VUE_APP_develope == "true") {
   // 开发环境
-  axios.defaults.baseURL = "http://192.168.0.105:8081/api";
+  // axios.defaults.baseURL = "http://192.168.0.105:8081/api";
+  axios.defaults.baseURL = "https://api.hust.online/drillbattle";
 } else {
   // 测试生产
-  axios.defaults.baseURL = "api.hust.online/drillbattle";
+  axios.defaults.baseURL = "https://api.hust.online/drillbattle";
 }
 
 // axios.defaults.baseURL = "http://127.0.0.1";
